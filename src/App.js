07c9535352526361b353c80f9b990765b8178bc5
vaskania <vnikabadze@unisens.ge>
2022-05-12 17:12:08 +0400

    added geolocation and swiper

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -20,6 +22,7 @@ const App = () => {
            <Route path='/' element={<Explore/>}/>
            <Route path='/offers' element={<Offers/>}/>
            <Route path='/category/:categoryName' element={<Category/>}/>
+
            <Route path='/profile' element={<PrivateRoute/>}>
              <Route path='/profile' element={<Profile/>}/>
            </Route>
@@ -27,6 +30,8 @@ const App = () => {
            <Route path='/sign-up' element={<SignUp/>}/>
            <Route path='/forgot-password' element={<ForgotPassword/>}/>
            <Route path='/create-listing' element={<CreateListing/>}/>
+           <Route path='/category/:categoryName/:listingId' element={<Listing/>}/>
+           <Route path='/contact/:landlordId' element={<Contact/>}/>
          </Routes>
          <Navbar/>
        </Router>
